6e48dae8fe2dfed273fe3e722a5dec6b0233ced8
coolcool5 <137592386+coolcool5@users.noreply.github.com>
2023-11-13 00:06:41 +0530

    Update clearConfirmationModal.tsx

--- components/clearConfirmationModal.tsx
+++ components/clearConfirmationModal.tsx
@@ -26,7 +26,7 @@ export function ClearConfirmation(props: {isOpen: boolean, onRequestClose: ()=>v
 				}}
 			>
 				<Typography style={{marginBottom: 10}} variant="h6">Are you sure?</Typography>
-				<Typography style={{marginBottom: 10}}>Clearing an entire table could erase a lot of data and could also make the creator really sad.</Typography>
+				<Typography style={{marginBottom: 10}}>Do you want to clear this chat?</Typography>
 				<Button variant="outlined" color="error" fullWidth disabled={loading} onClick={()=>{
 					setLoading(true);
 					props.submit(()=>{
